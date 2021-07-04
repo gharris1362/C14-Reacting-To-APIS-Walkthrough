f5ee7ce8a73138a4ccd7e4a28ac6264c03c9ceb2
gharris1362 <gharris1362@gmail.com>
2021-07-04 17:24:20 -0500

    walkthrough for the Reacting To Apis lab

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Films from './components/Films'
+import People from './components/People'
+
+const App = () => {
+
+    const [filmsLoaded, setFilmsLoaded] = useState()
+    const [peopleLoaded, setPeopleLoaded] = useState()
+
+    const loadFilms = () => {
+        console.log("Films Loaded")
+        setFilmsLoaded(true)
+        setPeopleLoaded(false)
+    }
+
+    const loadPeople = () => {
+        console.log("Films Loaded")
+        setPeopleLoaded(true)
+        setFilmsLoaded(false)
+    }
+
+    if (filmsLoaded == true) {
+        return (
+            <>
+                <button onClick={loadFilms}>Films</button>
+                <button onClick={loadPeople}>People</button>
+                <Films />
+            </>
+        )
+    } else if (peopleLoaded == true) {
+        return (
+            <>
+                <button onClick={loadFilms}>Films</button>
+                <button onClick={loadPeople}>People</button>
+                <People />
+            </>
+        )
+    } else {
+        return (
+            <>
+                <button onClick={loadFilms}>Films</button>
+                <button onClick={loadPeople}>People</button>
+                <h1>Neither are Loaded</h1>
+            </>
+        )
+    }
+}
+
+
+export default App
